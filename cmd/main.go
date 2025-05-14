94403cfbdabf906d248bcab07195ac31ba8c15a5
Sergej Zaporozhets <tataka18252@gmail.com>
2025-05-14 17:21:23 +0300

    Clean Architecture

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/chopic82region/todo-app"
 	"github.com/chopic82region/todo-app/pkg/handler"
+	"github.com/chopic82region/todo-app/pkg/repository"
+	"github.com/chopic82region/todo-app/pkg/service"
 )
 
 func main(){
-	handler := new(handler.Handler)
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handler := handler.NewHandler(services)
+	
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handler.InitRoutes()); err != nil{
 		log.Fatalf("error")
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,12 +1,17 @@
 package handler
 
 import (
-	
-
+	"github.com/chopic82region/todo-app/pkg/service"
 	"github.com/gin-gonic/gin"
 )
 
-type Handler struct{}
+type Handler struct{
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler{
+	return &Handler{services: services}
+}
 
 func (h *Handler) InitRoutes() *gin.Engine{
 	router := gin.New()
